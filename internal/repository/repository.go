28f4babff13d4d5adc352655f988129d796e15f1
Alexeyts0Y <tsoyalexey331@gmail.com>
2025-11-16 18:35:36 +0300

    #33 feat: implemented functionality for deactivating whole team members and reassigning all team's PRs

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 
 	errWrappers "github.com/Alexeyts0Y/TEST_TASK_AVITO/internal/errors"
@@ -30,6 +31,9 @@ type Repository interface {
 	ChooseRandomCandidates(candidates []string, count int) []string
 
 	GetReviewStats(ctx context.Context) ([]api.UserReviewStat, error)
+
+	DeactivateTeamMembers(ctx context.Context, teamName string) (int64, error)
+	ReassignPRsForTeam(ctx context.Context, teamName string) (api.ReassignmentSummary, error)
 }
 
 type PostgresRepository struct {
@@ -166,7 +170,7 @@ func (r *PostgresRepository) FindUserPullRequests(ctx context.Context, userId st
 	var pullRequestModels []model.PullRequest
 
 	searchPattern := "%" + userId + "%"
-	if err := r.DB.WithContext(ctx).Where("assigned_reviewers LIKE ?", searchPattern).Find(pullRequestModels).Error; err != nil {
+	if err := r.DB.WithContext(ctx).Where("assigned_reviewers LIKE ?", searchPattern).Find(&pullRequestModels).Error; err != nil {
 		return nil, err
 	}
 
@@ -235,4 +239,73 @@ func (r *PostgresRepository) GetReviewStats(ctx context.Context) ([]api.UserRevi
 	}
 
 	return stats, nil
+}
+
+func (r *PostgresRepository) DeactivateTeamMembers(ctx context.Context, teamName string) (int64, error) {
+	result := r.DB.WithContext(ctx).Model(&model.User{}).Where("team_name = ?", teamName).Update("is_active", false)
+
+	if result.Error != nil && errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return 0, fmt.Errorf("%w: команда с именем %s не найдена", errWrappers.ErrNotFound, teamName)
+	} else if result.Error != nil {
+		return 0, result.Error
+	}
+
+	return result.RowsAffected, nil
+}
+
+func (r *PostgresRepository) ReassignPRsForTeam(ctx context.Context, teamName string) (api.ReassignmentSummary, error) {
+	var pullRequests []model.PullRequest
+	var summary api.ReassignmentSummary
+	summary.TeamName = teamName
+
+	err := r.DB.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		rawQuery := `
+        SELECT pr.* 
+        FROM pull_requests pr
+        WHERE pr.status = 'OPEN'
+          AND EXISTS (
+            SELECT 1 
+            FROM users u 
+            WHERE u.team_name = ?
+              AND u.user_id = ANY(string_to_array(pr.assigned_reviewers, ','))
+          )`
+
+		err := tx.Raw(rawQuery, teamName).Scan(&pullRequests).Error
+		if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("%w: не найдены PR для команды %s", errWrappers.ErrNotFound, teamName)
+		} else if err != nil {
+			return fmt.Errorf("%w: ошибка при выборке PR для команды %s", err, teamName)
+		}
+
+		count := 0
+		for _, pr := range pullRequests {
+			excludeIds := strings.Split(pr.AssignedReviewers, ",")
+			candidates, err := r.FindActiveCandidates(ctx, teamName, excludeIds)
+			if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+				return fmt.Errorf("%w: не найдены PR для команды %s", errWrappers.ErrNotFound, teamName)
+			} else if err != nil {
+				return fmt.Errorf("%w: ошибка при поиске актвных кандидатов команды %s", err, teamName)
+			}
+
+			if len(candidates) == 0 {
+				continue
+			}
+
+			currentReviewersCount := len(strings.Split(pr.AssignedReviewers, ","))
+			chosen := r.ChooseRandomCandidates(candidates, currentReviewersCount)
+			if err := tx.Where("pull_request_id = ?", pr.PullRequestId).Model(&model.PullRequest{}).Update("assigned_reviewers", strings.Join(chosen, ",")).Error; err != nil {
+				return err
+			}
+			count++
+		}
+		summary.ReassignedPrsCount = count
+
+		return nil
+	})
+
+	if err != nil {
+		return api.ReassignmentSummary{}, err
+	}
+
+	return summary, nil
 }
